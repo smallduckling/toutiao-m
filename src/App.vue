51b0aecd49063dcfb9edd0dc03fb4928df8c2612
Your Name <17600238047@163.com>
2022-03-20 17:23:19 +0800

    项目初始化-调整出事结构目录

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <h1>黑马头条</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="less">
+</style>
